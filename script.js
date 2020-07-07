ef5415e190e51377ad331fa38b2db1b757aec76e
Alvin <alvinleung2009@gmail.com>
2020-07-06 19:40:37 -0700

    minor speed optimisation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
   setupHamburgerMenu();
   setupMenuHideOnScroll();
   setupProductGallery();
